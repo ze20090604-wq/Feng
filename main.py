c5860d6d6f2ae8cb99fa662555441f0a872119c8
楓紅 <ze20090604@gmail.com>
2026-04-29 16:46:57 +0800

    refactor(main): 調整 ChatOpenAI 初始化邏輯與環境變數讀取

--- main.py
+++ main.py
@@ -1,251 +1,32 @@
 import os
 from dotenv import load_dotenv
-from  langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 def main():
     load_dotenv()
     api_key = os.getenv("OPEN_API_KEY")
-    if  api_key :
-        print(f"open_API_KEY:已設定")
+    
+    if api_key:
+        print(f"open_API_KEY: 已設定")
     else:
-        print("open_API_KEY未設定") 
+        print("open_API_KEY 未設定") 
         return 
-    agant_name = "AI_hong"
-    print(f"Hello,我是{agant_name} ")
+    base_url = os.getenv("BASE_URL")
+    model = os.getenv("MODEL")
+    # agant_name = "AI_hong"
+    # print(f"Hello, 我是 {agant_name}")
+
+    llm = ChatOpenAI(
+        model=model,
+        temperature=0.6,
+        base_url=base_url,
+        api_key=api_key
+    )
+
+    user_message = "你好，可以回答我問題嗎？"
+    response = llm.invoke(user_message)
+
+    print(f"AI 回應內容:\n{response.content}")
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
